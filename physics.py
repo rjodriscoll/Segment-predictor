4e8284437b38f009aed62c2153601ea121d9346c
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-03-09 23:17:50 +0000

    update physics

--- physics.py
+++ physics.py
@@ -1,30 +1,115 @@
 import numpy as np
 
-grade = 9.2
-weight_rider = 75
-weight_else = 9
-weight_total = weight_rider + weight_else
-crr = 0.005 # rolling resistance
-rho = 0.076537 # air density
-a = 0.609 # frontal area
-cd = 0.8 # coef of drag
-loss_dt = 4 # drive train loss
+# we need to be able to calculate the power required for a speed. This is hard because velovity is needed for f_drag
+distance = 10
+time_mins = 55
+rider_weight = 75
+frontal_area = 0.609
+coef_drag = 0.7
+grade = 7
+weight_else = 8
+coef_rolling_resistance = 0.005
+air_density = 0.076537
+coef_drag = 0.7
+drive_losses = 4
 
 
+def get_power_given_speed(
+    grade: float,
+    rider_weight: float,
+    weight_else: float,
+    coef_rolling_resistance: float,
+    air_density: float,
+    frontal_area: float,
+    coef_drag: float,
+    drive_losses: float,
+    speed: float,
+):
 
-distance = 1.78 # km 
-time = (7/60)
-speed = distance /time
+    weight_total = rider_weight + weight_else
+    velocity = speed / 3.6
 
-v_kmh = speed 
-v =  v_kmh / 3.6 # velocity parameter, in m/s
+    f_gravity = 9.8067 * np.sinh(np.arctan(grade / 100)) * weight_total
+    f_rolling = (
+        9.8067
+        * np.cosh(np.arctan(grade / 100))
+        * weight_total
+        * coef_rolling_resistance
+    )
+    f_drag = 0.5 * coef_drag * frontal_area * air_density * velocity**2
+    f_resist = f_gravity + f_rolling + f_drag
+
+    power = (1 - drive_losses / 100) * f_resist * velocity
+    return power
 
 
-f_gravity = 9.8067 * np.sinh(np.arctan(grade/100)) * weight_total
-f_rolling = 9.8067 * np.cosh(np.arctan(grade/100)) * weight_total * crr
-f_drag = 0.5 * cd  * a * rho * v**2
-f_resist = f_gravity + f_rolling + f_drag 
-p_wheel = f_resist * v 
-p_legs = (1 - (loss_dt/100))**-1 * (f_resist) * v
+# the below function is needed to calculate the power required for a speed or a particular segment. This is becuase the user will provide one power and we will calculate the speed for that segment
+def converge_on_speed_given_power(target_power):
 
-print('power =', p_legs, 'speed =', v_kmh, 'cda =', cd * a )
+    lower, upper = target_power * 0.99, target_power * 1.01
+    speed_min, speed_max = 1, 100
+    speed = 1
+    power = 1
+
+    while not (power > lower and power < upper):
+
+        if power <= lower:
+            speed_min = speed
+            speed = np.mean([speed, speed_max])
+        if power >= upper:
+            speed_max = speed
+            speed = np.mean([speed, speed_min])
+
+        power = get_power_given_speed(
+            grade=grade,
+            rider_weight=rider_weight,
+            weight_else=weight_else,
+            coef_rolling_resistance=coef_rolling_resistance,
+            air_density=air_density,
+            frontal_area=frontal_area,
+            coef_drag=coef_drag,
+            drive_losses=drive_losses,
+            speed=speed,
+        )
+
+        if power >= lower and power <= upper:
+            return speed
+
+
+for p in range(200, 600, 5):
+    print('~' * 200)
+    print(p)
+    speed = converge_on_speed_given_power(p)
+    
+    power = get_power_given_speed(
+        grade=grade,
+        rider_weight=rider_weight,
+        weight_else=weight_else,
+        coef_rolling_resistance=coef_rolling_resistance,
+        air_density=air_density,
+        frontal_area=frontal_area,
+        coef_drag=coef_drag,
+        drive_losses=drive_losses,
+        speed=speed,
+    )
+   
+    time = distance/speed
+    time_seconds = 60 * 60 * time 
+    time_mins = time_seconds /60 
+    
+
+    print(
+        "power =",
+        round(power),
+        "speed =",
+        round(speed, 2),
+        "cda =",
+        round(coef_drag * frontal_area, 2),
+        "this rider covers",
+        distance,
+        "km in",
+        time_mins,
+        "mins"
+    )
+
+    print('~' * 200)
