5368258b542fce4d1f103e61be80121f2307e0ad
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-03-21 21:53:21 +0000

    add dash.py
    c

---
+++ dash.py
@@ -0,0 +1,16 @@
+import dash
+from dash import html
+from dash import dcc
+import plotly.express as px
+
+
+## stuff here 
+
+
+if __name__ == '__main__':
+    app = dash.Dash()
+
+
+
+
+    app.run_server(host='0.0.0.0') 
