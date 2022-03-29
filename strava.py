05975b312af906240195291e54e56c69a024a6e1
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-03-29 21:15:44 +0100

    as class

---
+++ strava.py
@@ -0,0 +1,20 @@
+
+
+class wkgCalculation:
+    def __init__(self, id : int):
+        self.id = id
+
+    def _is_already_processed(self):
+        pass
+
+    def _build_api_call(self):
+        pass
+
+    def _api_caller(self):
+        pass
+    
+    def _retrieve(self):
+        pass
+
+    def _plot(self):
+        pass
